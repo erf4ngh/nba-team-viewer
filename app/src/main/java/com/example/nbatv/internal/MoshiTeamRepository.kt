6d5b6c6891a7d90e70c126704c73ca8a3e0b9a88
Erfan Gh <erfanghafoori@gmail.com>
2022-01-21 16:04:09 -0500

    Retrofit Interface

--- app/src/main/java/com/example/nbatv/internal/MoshiTeamRepository.kt
+++ app/src/main/java/com/example/nbatv/internal/MoshiTeamRepository.kt
@@ -7,24 +7,17 @@ import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class RetrofitTeamRepository : TeamRepository {
+class MoshiTeamRepository : TeamRepository {
 
     override fun getAllTeams(): MutableList<TeamEntity> {
         var teamList : MutableList<TeamEntity> = mutableListOf<TeamEntity>()
 
-        val url = "https://raw.githubusercontent.com/scoremedia/nba-team-viewer/master"
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-
-        //retrofit interface call, not sure what to here
-
         val moshi = Moshi.Builder()
             .build()
-        val jsonAdapter: JsonAdapter<TeamEntity> = moshi.adapter<TeamEntity>()
+
+        val jsonAdapter: JsonAdapter<TeamEntity> = moshi.adapter
         teamList = jsonAdapter.fromJson(jsonFile) //need to still get this
+
         return teamList
     }
 }
--- app/src/main/java/com/example/nbatv/internal/retrofitApiService.kt
+++ app/src/main/java/com/example/nbatv/internal/retrofitApiService.kt
@@ -1,8 +1,26 @@
 package com.example.nbatv.internal
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 
 //maybe use this to make it cleaner
 interface retrofitApiService {
     @GET("/input.json")
+    fun getAllTeams() : Call<MutableList<TeamEntity>>
+
+    companion object{
+        val url = "https://raw.githubusercontent.com/scoremedia/nba-team-viewer/master"
+        fun create() : retrofitApiService {
+
+            // retrofit instance
+            val retrofit = Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+            return retrofit.create(retrofitApiService::class.java)
+        }
+    }
 }

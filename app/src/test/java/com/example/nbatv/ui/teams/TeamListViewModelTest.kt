d35acde2ee4ee4c047bfe25cf27877e9eaad11fe
Erfan Gh <erfanghafoori@gmail.com>
2022-01-26 12:20:03 -0500

    Setting up Koin Module + finished unit tests for TeamListViewModel
    still need to:
    -implement DI
    -mockito
    -add teamInfo (wins,loss,etc) to teamDetail

--- app/src/test/java/com/example/nbatv/ui/teams/TeamListViewModelTest.kt
+++ app/src/test/java/com/example/nbatv/ui/teams/TeamListViewModelTest.kt
@@ -8,32 +8,50 @@ import org.junit.Test
 
 class TeamListViewModelTest {
 
+    val teamsTest : List<Team> = listOf(
+        Team(1, "Toronto Raptors", 33, 12, emptyList()),
+        Team(2,"Utah Jazz", 34,34, emptyList()),
+        Team(3, "Atlanta Hawks", 54, 10, emptyList())
+    )
+    private val mockRepository = object: TeamRepository{
+        override fun getAllTeams(): List<Team>? {
+            return teamsTest
+        }
+    }
+    private val viewModel : TeamListViewModel = TeamListViewModel(mockRepository)
+
     @Test
     fun getAllTeams() {
-        val teamsTest : List<Team> = listOf(
-            Team(1, "Toronto Raptors", 33, 45, emptyList()),
-        )
-        val mockRepository = object: TeamRepository{
-            override fun getAllTeams(): List<Team>? {
-                return teamsTest
-            }
-        }
-        val viewModel : TeamListViewModel = TeamListViewModel(mockRepository)
         assertEquals(teamsTest, viewModel.getAllTeams())
     }
 
     @Test
     fun getTeamsSortedByName() {
-
+        val teamsTestSortedByName : List<Team> = listOf(
+            Team(3, "Atlanta Hawks", 54, 10, emptyList()),
+            Team(1, "Toronto Raptors", 33, 12, emptyList()),
+            Team(2,"Utah Jazz", 34,34, emptyList()),
+        )
+        assertEquals(teamsTestSortedByName, viewModel.getTeamsSortedByName())
     }
 
     @Test
     fun getTeamsSortedByWins() {
-
+        val teamsTestSortedByWins : List<Team> = listOf(
+            Team(3, "Atlanta Hawks", 54, 10, emptyList()),
+            Team(2,"Utah Jazz", 34,34, emptyList()),
+            Team(1, "Toronto Raptors", 33, 12, emptyList()),
+        )
+        assertEquals(teamsTestSortedByWins, viewModel.getTeamsSortedByWins())
     }
 
     @Test
     fun getTeamsSortedByLosses() {
-
+        val teamsTestSortedByLosses : List<Team> = listOf(
+            Team(2,"Utah Jazz", 34,34, emptyList()),
+            Team(1, "Toronto Raptors", 33, 12, emptyList()),
+            Team(3, "Atlanta Hawks", 54, 10, emptyList()),
+        )
+        assertEquals(teamsTestSortedByLosses, viewModel.getTeamsSortedByLosses())
     }
 }

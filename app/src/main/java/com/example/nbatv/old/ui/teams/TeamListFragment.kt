d6c5fc6772c2f7661eb3b87e68a44612eac9efde
erf4ngh <erfanghafoori@gmail.com>
2022-04-21 11:10:52 -0400

    Fix TeamListFragment, adapter for rv was not set

--- app/src/main/java/com/example/nbatv/old/ui/teams/TeamListFragment.kt
+++ app/src/main/java/com/example/nbatv/old/ui/teams/TeamListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.nbatv.old.ui.teams
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -33,11 +34,14 @@ class TeamListFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        recyclerViewTeams.layoutManager = LinearLayoutManager(activity)
+        recyclerViewTeams.adapter = adapter
         lifecycleScope.launch {
             viewModel.getAllTeams().distinctUntilChanged().collectLatest {
                 adapter.submitData(it)
             }
         }
+
     }
 
 
